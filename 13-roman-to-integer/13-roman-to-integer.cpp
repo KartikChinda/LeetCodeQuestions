7e68d270279b49178546633dbb1795a59729174c
Kartik Chinda <78233115+KartikChinda@users.noreply.github.com>
2022-03-29 09:54:52 +0530

    Time: 7 ms (87.71%), Space: 6 MB (88.08%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
+    int value( char r)
+    {
+        if(r=='I') return 1;
+        if(r=='V') return 5;
+        if(r=='X') return 10;
+        if(r== 'L') return 50;
+        if(r== 'C') return 100;
+        if(r=='D') return 500;
+        if(r=='M') return 1000;
+        
+    return -1;
+    }
     int romanToInt(string s) {
-        int res = 0;
-        for(int i = 0; i<s.length();i++){
-            char c = s[i];
-            if(c == 'I'){
-                if(i<s.length() && s[i+1]=='V'){
-                    res+=4;
+        int res=0;
+        for(int i=0;i<s.length();i++)
+        {
+            int s1=value(s[i]);
+            if(i+1 <s.length())
+            {
+                int s2= value(s[i+1]);
+                if(s1 >= s2)
+                {
+                    res= res +s1;
+                }
+                else
+                {
+                    res=res+s2-s1;
                     i++;
-                }else if(i<s.length() && s[i+1]=='X'){
-                    res+=9;
-                    i++;
-                }else{
-                    res++;
                 }
             }
-            else if(c == 'V') res+=5;
-            else if(c == 'X'){
-                if(i<s.length() && s[i+1]=='L'){
-                    res+=40;
-                    i++;
-                }else if(i<s.length() && s[i+1]=='C'){
-                    res+=90;
-                    i++;
-                }else{
-                    res+=10;
-                }
-            }
-            else if(c == 'L') res+=50;
-            else if(c == 'D') res+=500;
-            else if(c == 'M') res+=1000;
-            else if(c == 'C'){
-                if(i<s.length() && s[i+1]=='D'){
-                    res+=400;
-                    i++;
-                }else if(i<s.length() && s[i+1]=='M'){
-                    res+=900;
-                    i++;
-                }else{
-                    res+=100;
-                }
+            else
+            {
+                res=res+s1;
             }
         }
-        return res;
+    return res;
+        
     }
 };
