01ce8f508c0cf7e7c1d8d008e11a3913253a8b8d
Kartik Chinda <78233115+KartikChinda@users.noreply.github.com>
2022-10-19 15:03:51 +0530

    Added solution - LeetHub

---
+++ Length of the longest substring - GFG/length-of-the-longest-substring.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int longestUniqueSubsttr(string s){
+        int store[256]={0}; //array to store the occurences of all the characters
+        int l=0;    //left pointer
+        int r=0;    //right pointer
+        int ans=0;  //initializing the required length as 0
+        
+        while(r<s.length())     //iterate over the string till the right pointer reaches the end of the string 
+        {
+            store[s[r]]++;      //increment the count of the character present in the right pointer 
+            
+            while(store[s[r]]>1)    //if the occurence become more than 1 means the char is repeated
+            { 
+                store[s[l]]--;   //reduce the occurence of temp as it might be present ahead also in the string
+                l++;         //contraction of the present window till the occurence of the 't' char becomes 1
+            }
+            
+            ans = max(ans,r-l+1);    //As the index starts from 0 , ans will be (right pointer-left pointer + 1)
+            r++;        // now will increment the right pointer 
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string str;
+		cin>>str;
+		Solution ob;
+		cout<<ob.longestUniqueSubsttr(str)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
