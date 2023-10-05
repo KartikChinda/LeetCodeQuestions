0bee24c43f7c0517b329e83cab10e686c32bbae2
Kartik Chinda <78233115+KartikChinda@users.noreply.github.com>
2023-10-05 09:51:27 -0400

    Added solution - LeetHub

---
+++ Second Largest - GFG/second-largest.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	    sort(arr, arr+n, greater<int>());
+	    int largest = arr[0];
+	    for(int i = 1; i<n; i++){
+	        if(arr[i] < largest){
+	            return arr[i];
+	        }
+	    }
+	    return -1; 
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.print2largest(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
