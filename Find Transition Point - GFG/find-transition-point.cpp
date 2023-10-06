cd42acd20a0d2619a7494f87ded5d00834a9458d
Kartik Chinda <78233115+KartikChinda@users.noreply.github.com>
2023-10-06 10:10:33 -0400

    Added solution - LeetHub

--- Find Transition Point - GFG/find-transition-point.cpp
+++ Find Transition Point - GFG/find-transition-point.cpp
@@ -21,24 +21,15 @@ int main() {
 // } Driver Code Ends
 
 
-
+int soln = -1; 
 
 void firstOccuranceOf1(int arr[], int n, int st, int e){
     
-    
-    
-}
-
-int transitionPoint(int arr[], int n) {
-    // code here
-    if(n == 1 && arr[0] == 0) return -1; 
-    int res = -1; 
-    int st = 0, e = n-1; 
     while (st <= e){
         int mid = st + (e-st)/2;
         
         if(arr[mid] == 1){
-            res = mid; 
+            soln = mid; 
             e = mid-1;
         }else if (arr[mid] == 0){
             st = mid + 1; 
@@ -47,6 +38,12 @@ int transitionPoint(int arr[], int n) {
         }
     }
     
-    
-    return res; 
+}
+
+int transitionPoint(int arr[], int n) {
+    // code here
+    if(n == 1 && arr[0] == 0) return -1; 
+    soln = -1; 
+    firstOccuranceOf1(arr, n, 0, n-1);
+    return soln; 
 }
